fb14a424ee1671c91f3ede775cc54f0b59d4d6a2
U2A5F <U2A5F@outlook.com>
2023-10-02 12:43:02 +0800

    use fetch

--- scripts/net.ts
+++ scripts/net.ts
@@ -3,10 +3,10 @@ import { XMLParser } from 'npm:fast-xml-parser@4.3'
 
 let fetch_count = 0
 
-export async function fetchGet(url: URL | string) {
+export async function fetchGet(url: URL | Request | string, init?: RequestInit) {
     const fetch_id = fetch_count++
     core.info(`fetch ${fetch_id} GET -> ${url}`)
-    const res = await fetch(url)
+    const res = await fetch(url, init)
     if (res.ok) {
         core.info(`fetch ${fetch_id} GET <- ${res.status} ${res.statusText} ${res.type}`)
     } else {
@@ -15,8 +15,20 @@ export async function fetchGet(url: URL | string) {
     return res
 }
 
-export async function fetchXml<T>(url: URL | string): Promise<T> {
-    const res = await fetchGet(url)
+export async function fetchPost(url: URL | Request | string, init?: RequestInit) {
+    const fetch_id = fetch_count++
+    core.info(`fetch ${fetch_id} POST -> ${url}`)
+    const res = await fetch(url, { ...init ?? {}, method: 'POST' })
+    if (res.ok) {
+        core.info(`fetch ${fetch_id} POST <- ${res.status} ${res.statusText} ${res.type}`)
+    } else {
+        core.error(`fetch ${fetch_id} POST <- ${res.status} ${res.statusText} ${res.type}`)
+    }
+    return res
+}
+
+export async function fetchXml<T>(url: URL | Request | string, init?: RequestInit): Promise<T> {
+    const res = await fetchGet(url, init)
     const text = await res.text()
     const parser = new XMLParser()
     return parser.parse(text)
