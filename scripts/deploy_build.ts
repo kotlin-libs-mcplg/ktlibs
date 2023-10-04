daf0971ccb944f4bd0c574519557ea49ad3244a4
U2A5F <U2A5F@outlook.com>
2023-10-04 14:03:36 +0800

    artifact_name

--- scripts/deploy_build.ts
+++ scripts/deploy_build.ts
@@ -1,7 +1,7 @@
 import * as exec from 'npm:@actions/exec@1.1'
 import * as artifact from 'npm:@actions/artifact@1.1'
 import * as path from 'https://deno.land/std@0.203.0/path/mod.ts'
-import { project, project_and_version, version } from './get_project_and_version.ts'
+import { project, version } from './get_project_and_version.ts'
 import { Meta } from './types.ts'
 
 const meta_path = './meta.json'
@@ -9,7 +9,7 @@ const meta: Meta = JSON.parse(await Deno.readTextFile(meta_path))
 
 await exec.exec('./gradlew', [`:${project}:build`])
 
-const artifact_name = project_and_version
+const artifact_name = `${project}_${version}`
 const root_path = path.resolve(`./${project}/build/libs`)
 const files = [`${root_path}/${meta.build_output_prefix}${project}-${version}.jar`]
 
