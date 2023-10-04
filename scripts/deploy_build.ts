76c2e3b9a2637fed78edc162ad9a8f2aa3965bf9
U2A5F <U2A5F@outlook.com>
2023-10-04 14:27:12 +0800

    upload-artifact

--- scripts/deploy_build.ts
+++ scripts/deploy_build.ts
@@ -1,18 +1,17 @@
+import * as core from 'npm:@actions/core@1.10'
 import * as exec from 'npm:@actions/exec@1.1'
-import * as artifact from 'npm:@actions/artifact@1.1'
 import * as path from 'https://deno.land/std@0.203.0/path/mod.ts'
 import { project, version } from './get_project_and_version.ts'
-import { Meta } from './types.ts'
+// import { Meta } from './types.ts'
 
-const meta_path = './meta.json'
-const meta: Meta = JSON.parse(await Deno.readTextFile(meta_path))
+// const meta_path = './meta.json'
+// const meta: Meta = JSON.parse(await Deno.readTextFile(meta_path))
 
 await exec.exec('./gradlew', [`:${project}:build`])
 
 const artifact_name = `${project}_${version}`
 const root_path = path.resolve(`./${project}/build/libs`)
-const files = [`${root_path}/${meta.build_output_prefix}${project}-${version}.jar`]
+// const file = `${root_path}/${meta.build_output_prefix}${project}-${version}.jar`
 
-const artifact_client = artifact.create()
-
-await artifact_client.uploadArtifact(artifact_name, files, root_path)
+core.setOutput('artifact_name', artifact_name)
+core.setOutput('artifact_path', root_path)
